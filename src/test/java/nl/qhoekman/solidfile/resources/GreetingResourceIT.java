8cf918e6dce3456b8c921dc8bc28a314fc39bbb8
Quido Hoekman <quido@qhoekman.nl>
2024-03-09 13:31:07 +0100

    chore: initialize project

---
+++ src/test/java/nl/qhoekman/solidfile/resources/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package nl.qhoekman.solidfile.resources;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
